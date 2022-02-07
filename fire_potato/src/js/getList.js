e7166a2e3660fce9e17b8983133b8ab9e9d079dd
bacccine <bhm7266@naver.com>
2022-02-07 23:15:44 +0900

    bacccine: 일단 script 관심사의 분리란 걸 시도는 해 봄

--- fire_potato/src/js/getList.js
+++ fire_potato/src/js/getList.js
@@ -1,79 +1,87 @@
-fetch("http://185.162.75.92:3000/baccine")
-.then(response => {
-    return response.json();
-}).then(data =>{
-    const target = document.getElementById("target");
+/*
+관심사의 분리 기준: 역할
+GET으로 데이터를 가져와 화면에 뿌리는 과정에 대한 코드가 해당 파일에 있습니다.
+*/
 
-    const len = Object.keys(data).length;
-    for(let i =0;i<len;i++){
-        let list = "<tr class=\"inner-box\">";
-        list += "<th scope=\"row\">";
-        list += "<div class=\"event-date\">";
-        /*
-            날짜에 맞게 confirm하는 부분 관심사 분리
-            const { title, company, location, link, id } = data[index];
-        */
-        list += "<span>";
-        list += data[i].created_data.substring(8,10);
-        list += "</span>";
-        
-        const month = data[i].created_data.substring(5,7);
-
-        //한 번에 데이터들을 가져올 수 있다.
-        const { is_clear, created_by, name, order, created_data } = data[i];
-
-        list += "<p>";
-        if(month == 1) list+="January";
-        else if(month == 2) list+="Fabruary";
-        else if(month == 3) list+="March";
-        else if(month == 4) list+="April";
-        else if(month == 5) list+="May";
-        else if(month == 6) list+="June";
-        else if(month == 7) list+="July";
-        else if(month == 8) list+="August";
-        else if(month == 9) list+="September";
-        else if(month == 10) list+="October";
-        else if(month == 11) list+="November";
-        else if(month == 12) list+="December";
-        list += "</p>";
-
-        list += "</div>";
-        list += "</th>";
-        list += "<td>";
-        list += "<div class=\"event-wrap\">";
-        list += "<h3>";
-        list += data[i].name;
-        list += "</h3>";
-        
-        list += "<div class='meta'>";
-        list += "<div class=\"organizers\">";
-        list += data[i].created_by;
-        list += "</div>";
-        list += "</div>";
-        list += "</div>";
-        list += "</td>";
-
-        list += "<td>";
-        list += "<div class=\"r-no\">";
-        list += "<span>";
-        
-        list += "<label class=\"switch\">";
-        list += "<input type=\"checkbox\" onclick=\"stateUpdate(this)\" name=\""+i+"\"";
-        if(!data[i].is_clear) list += ">";
-        else list += "checked=\"checked\">";
-        
-        list += "<span class=\"slider round\"></span>";
-        list += "</label>";
-        
-        if(!data[i].is_clear) list += "<p>안머금</p>";
-        else list += "<p>머금</p>";
-        
-        list += "</span>";
-        list += "</div>";
-        list += "</td>";
-        list += "</tr>";
-        target.innerHTML+=list;
+// created_data를 달과 일로 나누는 함수: 관심사 분리---02.07 
+const date_split = (created_data) => created_data.substring(8,10);
+const month_split = (created_data) => {
+    switch(created_data.substring(5,7)) {
+        case(1): "January";
+        case(2): "Fabruary";
+        case(3): "March";
+        case(4): "April";
+        case(5): "May";
+        case(6): "June";
+        case(7): "July";
+        case(8): "August";
+        case(9): "September";
+        case(10): "October";
+        case(11): "November";
+        case(12): "December";
     }
-}).catch(err => {
-    alert("error!");
-});
+}
+
+// 먹킷리스트를 가져오는 함수
+const getAllList = async() => {
+    await fetch("http://185.162.75.92:3000/baccine")
+    .then(response => {
+        return response.json();
+    }).then(data =>{
+        const target = document.getElementById("target");
+
+        let list ="";
+        for(let i =0;i<data.length;i++){
+            const { is_clear, created_by, name, order, created_data } = data[i];
+            const day = date_split(created_data);
+            const month = month_split(created_data);
+
+            list += `
+                <tr class="inner-box">
+                    <th scope="row">
+                        <div class="event-date">
+                            <span>
+                                ${day}
+                            </span>
+                            <p>
+                                ${month}
+                            </p>
+
+                        </div>
+                    </th>
+                    <td>
+                        <div class="event-wrap">
+                            <h3>${name}</h3>
+                            <div class="meta">
+                                <div class="organizers">
+                                    ${created_by}
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="r-no">
+                            <span>
+                                <label class="switch">
+                                    <input type="checkbox" onclick="stateUpdate(this)" name=${order}`;
+
+                if(is_clear) list += ">";
+                else list += "checked='checked'>";
+            
+                list += `<span class="slider round"></span>
+                        </label>`;
+            
+                if(is_clear) list += "<p>안머금</p>";
+                else list += "<p>머금</p>";
+
+                list += `</span>
+                        </div>
+                    </td>
+                </tr>`;
+                            
+            target.innerHTML+=list;
+        }
+    }).catch(err => {
+        alert("error!");
+    });
+}
